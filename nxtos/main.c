b0f7a72bd07facede070f7784c2c8b594746e376
David Anderson <dave@natulte.net>
2007-04-22 02:21:43 +0000

    Comment and clean up the sound driver. Add synchronous playback functionality.

--- nxtos/main.c
+++ nxtos/main.c
@@ -26,18 +26,23 @@ main()
   core_init();
 
   avr_set_motor(0, 80, 0);
-  sound_freq(440, 1000);
+  sound_freq_async(440, 1000);
   systick_wait_ms(1000);
 
   avr_set_motor(0, -80, 0);
-  sound_freq(880, 1000);
+  sound_freq_async(880, 1000);
   systick_wait_ms(1000);
 
   avr_set_motor(0, 80, 0);
-  sound_freq(1320, 1000);
+  sound_freq_async(1320, 1000);
   systick_wait_ms(1000);
 
   avr_set_motor(0, 0, 1);
+
+  sound_freq(2000, 100);
+  systick_wait_ms(50);
+  sound_freq(2000, 100);
+  systick_wait_ms(50);
 
   avr_power_down();
 }
