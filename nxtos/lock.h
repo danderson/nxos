f1deb3848d76d66f5572dfe305976b06f6a0e3d7
David Anderson <dave@natulte.net>
2007-04-23 23:08:48 +0000

    Implement a non-blocking spinlock locking function.

--- nxtos/lock.h
+++ nxtos/lock.h
@@ -9,7 +9,9 @@ typedef volatile U8 spinlock; /* Basic spinlock type. */
 #define SPINLOCK_INIT_LOCKED 1
 
 extern void spinlock_acquire_from_ref(spinlock *lock);
-#define spinlock_acquire(lock) spinlock_acquire_from_ref(&lock);
+#define spinlock_acquire(lock) spinlock_acquire_from_ref(&lock)
+extern bool spinlock_try_acquire_from_ref(spinlock *lock);
+#define spinlock_try_acquire(lock) spinlock_try_acquire_from_ref(&lock)
 #define spinlock_release(lock) { lock = 0; }
 
 #endif /* __NXTOS_LOCK_H__ */
