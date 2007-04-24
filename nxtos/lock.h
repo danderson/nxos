5c277206718fbbe93f7e3c1e592ab8ed5898faf9
David Anderson <dave@natulte.net>
2007-04-24 01:33:16 +0000

    Add an atomic 32 bit CAS operation to the locking primitives.

--- nxtos/lock.h
+++ nxtos/lock.h
@@ -8,6 +8,8 @@ typedef volatile U8 spinlock; /* Basic spinlock type. */
 #define SPINLOCK_INIT_UNLOCKED 0
 #define SPINLOCK_INIT_LOCKED 1
 
+extern U32 atomic_cas32(U32 *dest, U32 val);
+
 extern void spinlock_acquire_from_ref(spinlock *lock);
 #define spinlock_acquire(lock) spinlock_acquire_from_ref(&(lock))
 extern bool spinlock_try_acquire_from_ref(spinlock *lock);
