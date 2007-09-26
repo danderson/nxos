a2a1f6f5392e4a11ed712c7bb513885b2be2e1ac
David Anderson <dave@natulte.net>
2007-09-27 00:58:53 +0200

    Couple the AVR and TWI drivers more tightly.
    This fixes a system lockup bug introduced by 5856ed87b0ed, due to the
    spinlocking and complex notification mechanism built into the TWI
    driver. That code was necessary on the assumption that something other
    than the coprocessor was on the I2C bus, which is false. So, this makes
    the code simpler, and forces the AVR and TWI drivers to work in
    lockstep, eliminating the risk of locking up.

--- nxos/drivers/twi.c
+++ nxos/drivers/twi.c
@@ -8,8 +8,6 @@
 #include "aic.h"
 #include "lock.h"
 
-static spinlock twi_busy = SPINLOCK_INIT_UNLOCKED;
-
 static volatile enum {
   TWI_UNINITIALISED = 0,
   TWI_FAILED,
@@ -21,8 +19,7 @@ static volatile enum {
 static volatile struct {
   U32 len;
   U8 *ptr;
-  bool *flag; /* Set *flag to TRUE when the pending request is complete. */
-} twi_request = { 0, NULL, NULL };
+} twi_request = { 0, NULL };
 
 
 void
@@ -41,9 +38,7 @@ twi_isr()
       }
       if (twi_request.len == 0) {
         *AT91C_TWI_IDR = ~0;
-        *twi_request.flag = TRUE;
 	twi_state = TWI_READY;
-        spinlock_release(twi_busy);
       }
     }
   }
@@ -60,9 +55,7 @@ twi_isr()
       twi_request.len--;
     } else {
       *AT91C_TWI_IDR = ~0;
-      *twi_request.flag = TRUE;
       twi_state = TWI_READY;
-      spinlock_release(twi_busy);
     }
   }
 
@@ -82,7 +75,7 @@ twi_isr()
 
 int
 twi_ready() {
-  return !twi_busy;
+  return twi_state == TWI_READY;
 }
 
 void
@@ -130,20 +123,16 @@ twi_init()
 
 
 void
-twi_read_async(U32 dev_addr, U8 *data, U32 nBytes, bool *flag)
+twi_read_async(U32 dev_addr, U8 *data, U32 nBytes)
 {
   U32 mode = ((dev_addr & 0x7f) << 16) | AT91C_TWI_IADRSZ_NO | AT91C_TWI_MREAD;
   U32 dummy;
-
-  spinlock_acquire(twi_busy);
 
   twi_state = TWI_RX_BUSY;
   *AT91C_TWI_IDR = ~0;	/* Disable all interrupts */
 
   twi_request.ptr = data;
   twi_request.len = nBytes;
-  twi_request.flag = flag;
-  *twi_request.flag = FALSE;
 
   dummy = *AT91C_TWI_SR;
   dummy = *AT91C_TWI_RHR;
@@ -157,19 +146,15 @@ twi_read_async(U32 dev_addr, U8 *data, U32 nBytes, bool *flag)
 }
 
 void
-twi_write_async(U32 dev_addr, U8 *data, U32 nBytes, bool *flag)
+twi_write_async(U32 dev_addr, U8 *data, U32 nBytes)
 {
   U32 mode = ((dev_addr & 0x7f) << 16) | AT91C_TWI_IADRSZ_NO;
-
-  spinlock_acquire(twi_busy);
 
   twi_state = TWI_TX_BUSY;
   *AT91C_TWI_IDR = ~0;	/* Disable all interrupts */
 
   twi_request.ptr = data;
   twi_request.len = nBytes;
-  twi_request.flag = flag;
-  *twi_request.flag = FALSE;
 
   *AT91C_TWI_MMR = mode;
   *AT91C_TWI_CR = AT91C_TWI_START | AT91C_TWI_MSEN;
