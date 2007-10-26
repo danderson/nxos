0411167caf9617953ad027aa282c8e5f6cd540f9
David Anderson <dave@natulte.net>
2007-10-26 18:57:38 +0200

    Rename main() to kernel_main().
    This is to better separate the kernel's entry point and the entry point of
    NxOS's payload.

--- nxos/kernel/main.c
+++ nxos/kernel/main.c
@@ -19,9 +19,13 @@
 
 #include "tests.h"
 
+/* main() is the entry point into the custom payload, not included in
+ * the NxOS core.
+ */
+extern void main();
+
 static void core_init() {
   aic_init();
-  interrupts_enable();
   systick_init();
   sound_init();
   avr_init();
@@ -30,6 +34,7 @@ static void core_init() {
   display_init();
   sensors_init();
   usb_init();
+  interrupts_enable();
 
   /* Delay a little post-init, to let all the drivers settle down. */
   systick_wait_ms(100);
@@ -41,6 +46,12 @@ static void core_shutdown() {
   avr_power_down();
 }
 
+void kernel_main() {
+  core_init();
+  main();
+  core_shutdown();
+}
+
 void test_task() {
   tests_usb();
   core_shutdown();
@@ -48,15 +59,12 @@ void test_task() {
 
 void main() {
   U32 *system_stack = NULL;
-  core_init();
 
   init_memory_pool(USERSPACE_SIZE, USERSPACE_START);
   system_stack = rtl_malloc(1024); // 1k stack
   run_first_task(test_task, system_stack + 1024);
 
   //tests_usb_hardcore();
-  //tests_usb();
+  tests_usb();
   //tests_all();
-
-  //core_shutdown();
 }
