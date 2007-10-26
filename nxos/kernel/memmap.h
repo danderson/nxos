ea84572e45c44da225318a31fc44aaa949a74f3e
David Anderson <dave@natulte.net>
2007-10-26 12:31:07 +0200

    Rewrite memmap.h to use proper pointer arithmetic and cast to the right data types everywhere.

--- nxos/kernel/memmap.h
+++ nxos/kernel/memmap.h
@@ -30,44 +30,48 @@ extern U8 __boot_from_samba__;
 /* Helper macro that converts a symbol value into a regular
  * integer. If we just addressed eg. __free_ram_start__ directly, the
  * C compiler would dereference and give us some random value (or a
- * data abort). If we just used eg. &__free_ram_start__, pointer
- * arithmetic would still apply.
+ * data abort).
  *
- * So, we need to grab the symbol's address, and then cast it to
- * U32. As the code to do this is a little ugly, it is defined once
- * here.
+ * So, we need to grab the symbol's address, and then cast it to a
+ * pointer to U8. That way, we have pointers pointing to the correct
+ * place.
  */
-#define SYMVAL(sym) ((U32)&(sym))
+#define SYMADDR(sym) ((U8*)&(sym))
+
+/* Section sizes requires a little ugly casting, so we define it once
+ * here as well.
+ */
+#define SECSIZE(start, end) ((U32)(end - start))
 
 /* Wrapping of the raw symbols into something usable by the rest of
  * the kernel. We also define a _SIZE constant for each section, which
  * is just the number of bytes it uses.
  */
-#define RAMTEXT_START SYMVAL(__ramtext_ram_start__)
-#define RAMTEXT_END SYMVAL(__ramtext_ram_end__)
-#define RAMTEXT_SIZE (RAMTEXT_END - RAMTEXT_START)
+#define RAMTEXT_START SYMADDR(__ramtext_ram_start__)
+#define RAMTEXT_END SYMADDR(__ramtext_ram_end__)
+#define RAMTEXT_SIZE SECSIZE(RAMTEXT_START, RAMTEXT_END)
 
-#define TEXT_START SYMVAL(__text_start__)
-#define TEXT_END SYMVAL(__text_end__)
-#define TEXT_SIZE (TEXT_END - TEXT_START)
+#define TEXT_START SYMADDR(__text_start__)
+#define TEXT_END SYMADDR(__text_end__)
+#define TEXT_SIZE SECSIZE(TEXT_START, TEXT_END)
 
-#define DATA_START SYMVAL(__data_ram_start__)
-#define DATA_END SYMVAL(__data_ram_end__)
-#define DATA_SIZE (DATA_END - DATA_START)
+#define DATA_START SYMADDR(__data_ram_start__)
+#define DATA_END SYMADDR(__data_ram_end__)
+#define DATA_SIZE SECSIZE(DATA_RAM_START, DATA_RAM_END)
 
-#define BSS_START SYMVAL(__bss_start__)
-#define BSS_END SYMVAL(__bss_end__)
-#define BSS_SIZE (BSS_END - BSS_START)
+#define BSS_START SYMADDR(__bss_start__)
+#define BSS_END SYMADDR(__bss_end__)
+#define BSS_SIZE SECSIZE(BSS_START, BSS_END)
 
-#define STACK_START SYMVAL(__stack_start__)
-#define STACK_END SYMVAL(__stack_end__)
-#define STACK_SIZE (STACK_END - STACK_START)
+#define STACK_START SYMADDR(__stack_start__)
+#define STACK_END SYMADDR(__stack_end__)
+#define STACK_SIZE SECSIZE(STACK_START, STACK_END)
 
-#define USERSPACE_START SYMVAL(__userspace_start__)
-#define USERSPACE_END SYMVAL(__userspace_end__)
-#define USERSPACE_SIZE (USERSPACE_END - USERSPACE_START)
+#define USERSPACE_START SYMADDR(__userspace_start__)
+#define USERSPACE_END SYMADDR(__userspace_end__)
+#define USERSPACE_SIZE SECSIZE(USERSPACE_START, USERSPACE_END)
 
-#define BOOT_FROM_SAMBA (SYMVAL(__boot_from_samba__) != NULL ? TRUE : FALSE)
+#define BOOT_FROM_SAMBA (SYMADDR(__boot_from_samba__) != NULL ? TRUE : FALSE)
 #define BOOT_FROM_ROM (!BOOT_FROM_SAMBA)
 
 #endif
