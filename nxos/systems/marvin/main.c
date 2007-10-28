b813fd9e590388a80a771a9aab7f7053a44e6974
David Anderson <dave@natulte.net>
2007-10-28 04:05:57 +0100

    Tweak Marvin to just boot a task and beep, to prove that it does something.
    It still crashes on shutdown for now.

--- nxos/systems/marvin/main.c
+++ nxos/systems/marvin/main.c
@@ -1,16 +1,14 @@
-#include "base/tests.h"
 #include "base/tlsf.h"
 #include "base/memmap.h"
+#include "base/drivers/sound.h"
 #include "task.h"
 
 void test_task() {
-  tests_usb();
+  sound_freq(440, 1000);
 }
 
 void main() {
   U32 *system_stack = NULL;
-  tests_usb();
-  return;
   init_memory_pool(USERSPACE_SIZE, USERSPACE_START);
   system_stack = rtl_malloc(1024); // 1k stack
   run_first_task(test_task, system_stack + 1024);
