7c94faac762fa4c76a73b6ad4229683e786e4e31
Maxime Petazzoni <maxime.petazzoni@bulix.org>
2007-10-30 15:58:34 +0100

    Updated driver for the NXT Ultrasonic radar, using the new I2C code.
    New version of the radar's driver. Supports most of the radar
    features, including:
     - detection of radar presence on the I2C bus
     - radar warm reset
     - getters and setters for all values
    
    Todo: provide a simple API to the one-shot and event operation
    modes. If possible, make the radar_read_all() function work.

--- nxos/drivers/radar.c
+++ nxos/drivers/radar.c
@@ -13,20 +13,15 @@
 
 #include "mytypes.h"
 #include "nxt.h"
-#include "avr.h"
 #include "interrupts.h"
-#include "systick.h"
-#include "sensors.h"
 #include "display.h"
-#include "i2c.h"
+#include "systick.h"
+#include "util.h"
 #include "i2c_memory.h"
-#include "usb.h"
-#include "sound.h"
+#include "radar.h"
 
-/* As defined in the NXT Hardware Developer Kit, the Ultrasonic sensor
- * has been given address 1 (within a 7 bit context).
- */
-#define RADAR_I2C_ADDRESS 0x1
+/* LEGO's Radar factory sensor type. */
+#define RADAR_LEGO_SENSOR_TYPE "Sonar"
 
 /** Radar's internal memory addresses.
  *
@@ -34,17 +29,15 @@
  * radar parameters and readings.
  */
 typedef enum {
-  RADAR_VERSION = 0x00,
-
-  RADAR_PRODUCT_ID = 0x08,
-
-  RADAR_SENSOR_TYPE = 0x10,
+  RADAR_VERSION,
+  RADAR_PRODUCT_ID,
+  RADAR_SENSOR_TYPE,
   RADAR_FACTORY_ZERO,
   RADAR_FACTORY_SCALE_FACTOR,
   RADAR_FACTORY_SCALE_DIVISOR,
   RADAR_MEASUREMENT_UNITS,
 
-  RADAR_INTERVAL = 0x40,
+  RADAR_INTERVAL,
   RADAR_OP_MODE,
   RADAR_R0,
   RADAR_R1,
@@ -57,130 +50,247 @@ typedef enum {
   RADAR_CURRENT_ZERO,
   RADAR_CURRENT_SCALE_FACTOR,
   RADAR_CURRENT_SCALE_DIVISOR,
-} radar_commands;
+  
+  RADAR_N_COMMANDS,
+} radar_memory_slot;
 
-extern U32 offset;
-extern U8 dump[1024];
-extern bool record;
+/** Radar commands.
+ *
+ * This array of structures is mapped by the radar_memory_slots enum
+ * (see radar.h) and links the address in the radar's memory for the given
+ * value to its returned data length.
+ */
+static struct radar_cmd_info {
+  U8 addr;
+  U8 len;
+} radar_cmds[RADAR_N_COMMANDS] = {
+  { 0x00, 8 }, // RADAR_VERSION: V1.0
+  { 0x08, 8 }, // RADAR_PRODUCT_ID: LEGO
+  { 0x10, 8 }, // RADAR_SENSOR_TYPE: Sonar
+  { 0x11, 1 }, // RADAR_FACTORY_ZERO: 0x00
+  { 0x12, 1 }, // RADAR_FACTORY_SCALE_FACTOR: 0x01
+  { 0x13, 1 }, // RADAR_FACTORY_SCALE_DIVISOR: 0x0E
+  { 0x14, 7 }, // RADAR_MEASUREMENT_UNITS: 10E-2m
+  
+  { 0x40, 1 }, // RADAR_INTERVAL: Interval
+  { 0x41, 1 }, // RADAR_OP_MODE: { 0x00, 0x01, 0x02, 0x03, 0x04 }
+  
+  // RADAR_R0 ... RADAR_R7: Radar readings
+  { 0x42, 1 }, { 0x43, 1 }, { 0x44, 1 }, { 0x45, 1 },
+  { 0x46, 1 }, { 0x47, 1 }, { 0x48, 1 }, { 0x49, 1 },
+
+  { 0x50, 1 }, // RADAR_CURRENT_ZERO: Zero
+  { 0x51, 1 }, // RADAR_CURRENT_SCALE_FACTOR: Scale factor
+  { 0x52, 1 }, // RADAR_CURRENT_SCALE_DIVISOR: Scale divisor
+};
 
 /** Initializes the radar sensor in LEGO compatibility mode. */
-void radar_init(U8 sensor)
-{
+void radar_init(U8 sensor) {
   i2c_memory_init(sensor, RADAR_I2C_ADDRESS, TRUE);
 }
 
-void radar_display_lines(U8 sensor)
-{
-  U32 lines = *AT91C_PIOA_PDSR;
-
-  sensor_pins pins = sensors_get_pins(sensor);
-  display_string("[");
-  display_uint(lines & pins.sda ? 1 : 0);
-  display_string("/");
-  display_uint(lines & pins.scl ? 1 : 0);
-  display_string("]\n");
+/** Reads a value from the radar's memory slot into the provided buffer. */
+static bool radar_read(U8 sensor, radar_memory_slot slot, U8 *val) {
+  struct radar_cmd_info *cmd = &radar_cmds[slot];
+  return i2c_memory_read(sensor, cmd->addr, val, cmd->len) == I2C_ERR_OK;
 }
 
-void radar_send_dump() {
-  display_string("dumping... ");
-
-  usb_send((U8 *) (&offset), 4);
-  while (usb_can_send());
-
-  usb_send(dump, offset);
-  while (usb_can_send());
-
-  display_string("done.\n");
+/** Returns a value from the radar's memory.
+ *
+ * Defaults to 0xFF if the read failed.
+ */
+static U8 radar_read_value(U8 sensor, radar_memory_slot slot) {
+  U8 value;
+  return radar_read(sensor, slot, &value) ? value : 0xFF;
 }
 
-void radar_test(U8 sensor)
-{
-  U8 product_id[8] = { 0x0 };
-  i2c_txn_err err;
+/** Writes the given buffer into the radar's memory slot. */
+static bool radar_write(U8 sensor, radar_memory_slot slot, U8 *val) {
+  struct radar_cmd_info *cmd = &radar_cmds[slot];
+  return i2c_memory_write(sensor, cmd->addr, val, cmd->len) == I2C_ERR_OK;
+}
 
-  display_clear();
-  display_cursor_set_pos(0, 0);
-  display_string("<? \n");
+/** Radar detection.
+ *
+ * Tries to detect the presence of a compatible radar on the given port by
+ * reading the device's sensor type and comparing it to the default sensor
+ * type of the LEGO-manufactured radar, "Sonar".
+ *
+ * Returns TRUE if a compatible radar was found.
+ */
+bool radar_detect(U8 sensor) {
+  U8 type[8] = { 0x0 };
   
-  err = i2c_memory_read(sensor, RADAR_PRODUCT_ID, product_id, 8);
+  return radar_read(sensor, RADAR_SENSOR_TYPE, type)
+    && strcmp((char *)type, RADAR_LEGO_SENSOR_TYPE) == 0;
+}  
 
-  systick_wait_ms(1000);
-  display_string("> err: ");
-  display_uint(err);
+/** High-level radar control interface. */
+
+/** Resets the radar connected to the given sensor port.
+ *
+ * Requests a warm radar reset, and reset all parameters to factory
+ * defaults.
+ */
+void radar_reset(U8 sensor) {
+  U8 reset = RADAR_OP_RESET;
+  U8 val = 0x0;
+
+  /* Do a warm reset and wait a little bit. */
+  radar_write(sensor, RADAR_OP_MODE, &reset);
+  systick_wait_ms(100);
+  
+  /* Reset zero, scale factor and scale divisor to factory values. */
+  if (radar_read(sensor, RADAR_FACTORY_ZERO, &val)) {
+    radar_write(sensor, RADAR_CURRENT_ZERO, &val);
+  }
+  
+  if (radar_read(sensor, RADAR_FACTORY_SCALE_FACTOR, &val)) {
+    radar_write(sensor, RADAR_CURRENT_SCALE_FACTOR, &val);
+  }
+
+  if (radar_read(sensor, RADAR_FACTORY_SCALE_DIVISOR, &val)) {
+    radar_write(sensor, RADAR_CURRENT_SCALE_DIVISOR, &val);
+  }
+}
+
+
+/** Radar getters. */
+
+bool radar_get_version(U8 sensor, U8 *version) {
+  return radar_read(sensor, RADAR_VERSION, version);
+}
+
+bool radar_get_product_id(U8 sensor, U8 *product_id) {
+  return radar_read(sensor, RADAR_PRODUCT_ID, product_id);
+}
+
+bool radar_get_sensor_type(U8 sensor, U8 *sensor_type) {
+  return radar_read(sensor, RADAR_SENSOR_TYPE, sensor_type);
+}
+
+U8 radar_get_factory_zero(U8 sensor) {
+  return radar_read_value(sensor, RADAR_FACTORY_ZERO);
+}
+
+U8 radar_get_factory_scale_factor(U8 sensor) {
+  return radar_read_value(sensor, RADAR_FACTORY_SCALE_FACTOR);
+}
+
+U8 radar_get_factory_scale_divisor(U8 sensor) {
+  return radar_read_value(sensor, RADAR_FACTORY_SCALE_DIVISOR);
+}
+
+bool radar_get_measurement_units(U8 sensor, U8 *units) {
+  return radar_read(sensor, RADAR_MEASUREMENT_UNITS, units);
+}
+
+U8 radar_get_interval(U8 sensor) {
+  return radar_read_value(sensor, RADAR_INTERVAL);
+}
+
+U8 radar_get_op_mode(U8 sensor) {
+  return radar_read_value(sensor, RADAR_OP_MODE);
+}
+
+/** Returns radar's measurement #object from the given sensor number.
+ *
+ * To emit a beep which freq ranges from approx 8 kHz for smallest distances
+ * to 380 Hz for greater detected distances (up to ??cm), you can use:
+ *   sound_freq_async(8000 - r0 * 30, 100);
+ *
+ * Note: a return value of 0x00 means that no object was detected. A value
+ * of 0xFF means that the read failed.
+ */
+U8 radar_read_distance(U8 sensor, S8 object) {
+  return radar_read_value(sensor, RADAR_R0+object);
+}
+
+/** Read all radar's measurements at once.
+ *
+ * The given buffer 'buf' must be pre-allocated to hold the eight measurements
+ * of the radar.
+ *
+ * Note: DOES NOT WORK (is it even possible to read as many bytes we want?)
+ */
+bool radar_read_all(U8 sensor, U8 *buf) {
+  /* We use the low-level i2c_memory_read function here to try to read
+   * all measurements at once.
+   */
+  return i2c_memory_read(sensor, radar_cmds[RADAR_R0].addr,
+                         buf, 8) == I2C_ERR_OK;
+}
+
+U8 radar_get_current_zero(U8 sensor) {
+  return radar_read_value(sensor, RADAR_CURRENT_ZERO);
+}
+
+U8 radar_get_current_scale_factor(U8 sensor) {
+  return radar_read_value(sensor, RADAR_CURRENT_SCALE_FACTOR);
+}
+
+U8 radar_get_current_scale_divisor(U8 sensor) {
+  return radar_read_value(sensor, RADAR_CURRENT_SCALE_DIVISOR);
+}
+
+
+/** Radar setters. */
+
+bool radar_set_interval(U8 sensor, U8 interval) {
+  return radar_write(sensor, RADAR_INTERVAL, &interval);
+}
+
+/** Sets the radar operation mode. See radar.h for available modes. */
+bool radar_set_op_mode(U8 sensor, U8 mode) {
+  return radar_write(sensor, RADAR_OP_MODE, &mode);
+}
+
+bool radar_set_current_zero(U8 sensor, U8 zero) {
+  return radar_write(sensor, RADAR_CURRENT_ZERO, &zero);
+}
+
+bool radar_set_current_scale_factor(U8 sensor, U8 factor) {
+  return radar_write(sensor, RADAR_CURRENT_SCALE_FACTOR, &factor);
+}
+
+bool radar_set_current_scale_divisor(U8 sensor, U8 divisor) {
+  return radar_write(sensor, RADAR_CURRENT_SCALE_DIVISOR, &divisor);
+}
+
+
+
+/** Display connected radar's information. */
+void radar_info(U8 sensor) {
+  U8 buf[8];
+
+  // Product ID (LEGO)
+  memset(buf, 0, 8);
+  radar_get_product_id(sensor, buf);
+  display_string((char *)buf);
+  display_string(" ");
+
+  // Sensor Type (Sonar)
+  memset(buf, 0, 8);
+  radar_get_sensor_type(sensor, buf);
+  display_string((char *)buf);
+  display_string(" ");
+  
+  // Version (V1.0)
+  memset(buf, 0, 8);
+  radar_get_version(sensor, buf);
+  display_string((char *)buf);
   display_end_line();
 
-  if (*product_id) {
-    display_string(">> ");
-    display_string((char *)product_id);
-    display_end_line();
-  } else {
-    display_string("ERROR\n");
-  }
-
-  while (avr_get_button() != BUTTON_OK);
-  display_clear();
-  display_cursor_set_pos(0, 0);
-  radar_send_dump();
-}
-
-bool radar_info(U8 sensor)
-{
-  U8 product_id[8] = { 0x0 };
-  U8 sensor_type[8] = { 0x0 };
-  U8 version[8] = { 0x0 };
-
-  display_clear();
-  display_cursor_set_pos(0, 0);
-  display_string("Searching radar...\n");
-
-  /* Read radar factory information. */
-  i2c_memory_read(sensor, RADAR_PRODUCT_ID, product_id, 8);
-  i2c_memory_read(sensor, RADAR_SENSOR_TYPE, sensor_type, 8);
-  i2c_memory_read(sensor, RADAR_VERSION, version, 8);
-
-  if (*product_id && *sensor_type && *version) {
-    display_string("Found!\n\n");
-
-    display_string((char *)sensor_type);
-    display_string(" ");
-    display_string((char *)version);
-    display_end_line();
-
-    display_string("    by ");
-    display_string((char *)product_id);
-    display_end_line();
-
-    return TRUE;
-  }
-
-  display_string("Error!\n");
-  display_string("(OK to try again)\n");
-  return FALSE;
-}
-
-void radar_read_r0(U8 sensor)
-{
-  U8 r0 = 0x00;
-
-  display_clear();
-  display_cursor_set_pos(0, 0);
-  display_string("Reading R0...\n");
-  display_string("(cancel to halt)\n\n");
-
-  /* Read first measurement */
-  i2c_memory_read(sensor, RADAR_R0, &r0, 1);
+  // Measurement units
+  display_string("Units: ");
+  memset(buf, 0, 8);
+  radar_get_measurement_units(sensor, buf);
+  display_string((char *)buf);
+  display_end_line();
   
-  display_string(">> ");
-
-  if (r0 < 0xFF) {
-    display_uint(r0);
-    display_string(" cm\n");
-
-    /* Emit a beep, range goes from approx 8 kHz for smallest distances
-     * to 380 Hz for greater detected distances (up to ??cm).
-     */
-    sound_freq_async(8000 - r0 * 30, 100);
-  } else {
-    display_string("n/a\n");
-  }
+  // Measurement interval
+  display_string("Interval: ");
+  display_uint(radar_get_interval(sensor));
+  display_string(" ms?\n");
 }
+
--- nxos/drivers/radar.h
+++ nxos/drivers/radar.h
@@ -3,9 +3,50 @@
 
 #include "mytypes.h"
 
+/* As defined in the NXT Hardware Developer Kit, the Ultrasonic sensor
+ * has been given address 1 (within a 7 bit context).
+ */
+#define RADAR_I2C_ADDRESS  0x01
+
+#define RADAR_DEFAULT_INTERVAL 1
+
+/* Radar operation modes: these are values that can be set at the
+ * RADAR_OP_MODE (see below) to change the radar's behavior.
+ */
+#define RADAR_OP_OFF       0x00
+#define RADAR_OP_SINGLE    0x01
+#define RADAR_OP_CONTINOUS 0x02
+#define RADAR_OP_EVENT     0x03
+#define RADAR_OP_RESET     0x04
+
 void radar_init(U8 sensor);
-bool radar_info(U8 sensor);
-void radar_test(U8 sensor);
-void radar_read_r0(U8 sensor);
+bool radar_detect(U8 sensor);
+void radar_reset(U8 sensor);
+void radar_info(U8 sensor);
+
+/** Getters for static values. */
+bool radar_get_version(U8 sensor, U8 *version);
+bool radar_get_product_id(U8 sensor, U8 *product_id);
+bool radar_get_sensor_type(U8 sensor, U8 *sensor_type);
+U8 radar_get_factory_zero(U8 sensor);
+U8 radar_get_factory_scale_factor(U8 sensor);
+U8 radar_get_factory_scale_divisor(U8 sensor);
+bool radar_get_measurement_units(U8 sensor, U8 *units);
+
+/** Getters for dynamic values and measurements */
+U8 radar_get_interval(U8 sensor);
+U8 radar_get_op_mode(U8 sensor);
+U8 radar_read_distance(U8 sensor, S8 object);
+bool radar_read_all(U8 sensor, U8 *buf);
+U8 radar_get_current_zero(U8 sensor);
+U8 radar_get_current_scale_factor(U8 sensor);
+U8 radar_get_current_scale_divisor(U8 sensor);
+
+/** Setters to configure the radar. */
+bool radar_set_interval(U8 sensor, U8 interval);
+bool radar_set_op_mode(U8 sensor, U8 mode);
+bool radar_set_current_zero(U8 sensor, U8 zero);
+bool radar_set_current_scale_factor(U8 sensor, U8 factor);
+bool radar_set_current_scale_divisor(U8 sensor, U8 divisor);
 
 #endif
