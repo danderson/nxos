c025e3973c81185e98b64c1b73b370199b3f7015
David Anderson <dave@natulte.net>
2007-11-02 23:01:31 +0100

    Replace swap_task_stacks with a getter and a setter.

--- nxos/systems/marvin/task.h
+++ nxos/systems/marvin/task.h
@@ -5,7 +5,7 @@
 
 extern void run_first_task(void (task)(void), U32 *stack);
 extern U32 *swap_task_stacks(U32 *new_stack);
-extern volatile U32 *get_system_stack();
-extern U32 get_cpsr();
+extern U32 *get_system_stack();
+extern void set_system_stack(U32 *stack);
 
 #endif /* __NXOS_TASK_H__ */
