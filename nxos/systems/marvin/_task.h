2c02516f08f1defad661b251a4daa8fe75ec1dc8
David Anderson <dave@natulte.net>
2007-11-14 23:29:18 +0100

    Implement assembler functions required by the scheduler.

---
+++ nxos/systems/marvin/_task.h
@@ -0,0 +1,22 @@
+/** @file _task.h
+ *  @brief Assembler task access helpers for the scheduler.
+ */
+
+/* Copyright (C) 2007 the NxOS developers
+ *
+ * See AUTHORS for a full list of the developers.
+ *
+ * Redistribution of this file is permitted under
+ * the terms of the GNU Public License (GPL) version 2.
+ */
+
+#ifndef __NXOS_MARVIN__TASK_H__
+#define __NXOS_MARVIN__TASK_H__
+
+#include "base/types.h"
+
+void mv__task_run_first(nx_closure_t func, U32 *stack);
+U32 *mv__task_get_stack();
+void mv__task_set_stack(U32 *stack);
+
+#endif /* __NXOS_MARVIN__TASK_H__ */
