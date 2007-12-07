7adfad92645dab78324ceda9fb37fcb342fae7e0
David Anderson <dave@natulte.net>
2007-12-07 15:33:42 +0100

    Remove garbage from list.h and add more functions.

--- nxos/systems/marvin/list.h
+++ nxos/systems/marvin/list.h
@@ -98,78 +98,28 @@
 
 /** Remove and return the head of @a list */
 #define mv_list_pop_head(list) ({ \
+  typeof(list) __ret_elt; \
   if (list) \
-    mv_list_pop(list, list); \
+    __ret_elt = mv_list_pop(list, list); \
   else \
-    
+    __ret_elt = NULL; \
+  __ret_elt; \
+})
 
 /** Remove and return the tail of @a list */
+#define mv_list_pop_tail(list) ({ \
+  typeof(list) __ret_elt; \
+  if (list) \
+    __ret_elt = mv_list_pop(list, (list)->prev);        \
+  else \
+    __ret_elt = NULL; \
+  __ret_elt; \
+})
+
+/** Rotate @a list forward. */
+#define mv_list_rotate_forward(list) ({ \
+  if (list) \
+    (list) = (list)->next; \
+})
 
 #endif /* __NXOS_MARVIN_LIST_H__ */
-
-Fall 2007 (indicative, no results yet):
-TX52 (Personal Project - NxOS embedded operating system)
-RE51 (Distributed Systems)
-IA52 (Natural Language Processing)
-IA54 (Multi-Agent Systems)
-AG51 (Advanced Algorithms)
-TR52 (Realtime Operating Systems)
-HE09 (History of Science)
-
-Spring 2007:
-TX52 - A (Personal Project - NxOS embedded operating system)
-LO51 - A (Systems and Database Administration)
-LO52 - A (Embedded and mobile computing)
-TR54 - A (Applications of Realtime Systems)
-RE56 - A (Mobile Networking)
-DR05 - A (Intellectual Property Law)
-LE06 - A (American Civilization)
-
-Fall 2006: (intern at Google)
-
-Spring 2006:
-MI43 - A (Microcontrollers and Embedded Programming)
-LO41 - A (Operating System Fundamentals)
-IN41 - B (Digital Signal Processing)
-RE41 - B (Networking Fundamentals)
-AG41 - A (Heuristics and Operational Research)
-GO01 - C (Current Geopolitics)
-
-Fall 2005:
-IA41 - A (Artificial Intelligence and Data Representation)
-BD40 - D (Introduction to databases)
-MI41 - A (Digital Logic and Microprocessors)
-LO43 - A (Introduction to Object-Oriented Programming)
-AR01 - A (Art and Society)
-LE04 - A (British History)
-
-Spring 2005:
-PH01 - C (Introduction to philosophy)
-PS26 - C (Electromagnetism)
-SY20 - E (Introduction to automatism)
-TX20 - A (Personal project - a highly distributed online world)
-MT26 - F (Series and functions of complex variables)
-
-Fall 2004:
-CE01 - A (Communication skills in English)
-LO21 - B (Algorithmics and programming II)
-LO22 - A (Introduction to Unix and Unix C programming)
-MT25 - C (Applications of algebra and analysis to geometry)
-PS27 - B (Thermodynamics)
-SQ20 - B (Statistics and probability)
-
-Spring 2004:
-EC01 - B (Introduction to economy)
-GE01 - C (Introduction to management and accounting)
-LO10 - A (Introduction to computing)
-LO11 - A (Algorithmics and programming I)
-MT12 - C (Integration, linear algebra and functions of multiple variables)
-PS11 - B (Point mechanics and geometric optics)
-
-Fall 2003
-MT11 - D (Basic algebra and analysis)
-PS12 - C (Measurements and Electricity)
-CM11 - C (General Chemistry)
-TN10 - C (Mechanical Construction)
-MP10 - B (Introduction to scientific reasoning)
-LS00 - B (Introduction to Spanish)
