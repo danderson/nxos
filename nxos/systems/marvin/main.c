47ac9a1b379be82a695d47fbc7342187c0f5de37
David Anderson <dave@natulte.net>
2007-12-10 04:30:09 +0100

    Make Marvin's main.c into a comprehensive scheduler test.
    The main routine now tests semaphores, sleeping and multitasking.

--- nxos/systems/marvin/main.c
+++ nxos/systems/marvin/main.c
@@ -14,6 +14,7 @@
 
 #include "marvin/_scheduler.h"
 #include "marvin/semaphore.h"
+#include "marvin/time.h"
 
 mv_sem_t *beep_res;
 
@@ -26,8 +27,36 @@ static void beep_consumer() {
 
 static void beep_producer() {
   while(1) {
-    nx_systick_wait_ms(2000);
+    mv_time_sleep(2000);
     mv_semaphore_inc(beep_res);
+  }
+}
+
+U32 sleep_iter = 0, sleep_time = 0, wakeup_time = 0;
+
+void test_display() {
+  U32 counter = 0;
+  nx_display_clear();
+  while(1) {
+    counter++;
+    nx_display_cursor_set_pos(0,0);
+    nx_display_hex(counter);
+    nx_display_end_line();
+    nx_display_hex(sleep_iter);
+    nx_display_end_line();
+    nx_display_uint(sleep_time);
+    nx_display_string(" / ");
+    nx_display_uint(wakeup_time);
+  }
+}
+
+void test_sleep() {
+  while(1) {
+    mv_time_sleep(100);
+    sleep_time = nx_systick_get_ms();
+    mv_time_sleep(100);
+    wakeup_time = nx_systick_get_ms();
+    sleep_iter++;
   }
 }
 
@@ -37,5 +66,7 @@ void main() {
   beep_res = mv_semaphore_create(0);
   mv_scheduler_create_task(beep_consumer, 512);
   mv_scheduler_create_task(beep_producer, 512);
+  mv_scheduler_create_task(test_display, 512);
+  mv_scheduler_create_task(test_sleep, 512);
   mv__scheduler_run();
 }
