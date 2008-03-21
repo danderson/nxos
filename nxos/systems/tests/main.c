726b816eda89d35ca4bafd73db5039b17c46d862
Maxime Petazzoni <maxime.petazzoni@bulix.org>
2008-03-21 10:42:00 +0100

    Put the USB console test back as the default test in the tests appkernel.

--- nxos/systems/tests/main.c
+++ nxos/systems/tests/main.c
@@ -18,8 +18,8 @@ void main(void) {
   nx_systick_install_scheduler(security_hook);
 
   //tests_all();
-  //tests_usb();
-  tests_bt();
+  tests_usb();
+  //tests_bt();
   //tests_usb_hardcore();
   //tests_radar();
 }
