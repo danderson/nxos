8c1d07a090fee5c71a1861af88ffbd938d904c03
Maxime Petazzoni <maxime.petazzoni@bulix.org>
2008-04-03 11:06:05 +0200

    Flash read/write demo.

--- nxos/systems/tag-route/main.c
+++ nxos/systems/tag-route/main.c
@@ -11,21 +11,50 @@
 #include "base/drivers/systick.h"
 #include "base/drivers/motors.h"
 
+/* HACK */
+#include "base/drivers/_efc.h"
+
 #define ROUTE_FILE "tag_route.data"
 
 void main(void) {
-  fs_fd_t store;
+  U32 data[EFC_PAGE_WORDS] = {0}, num = 128;
+  U32 t = (EFC_WRITE_KEY << 24) + ((num & 0x000003FF) << 8) + EFC_CMD_WP;
+
+  nx__efc_init();
 
   nx_display_clear();
-  nx_fs_init();
 
-  if (nx_fs_open(ROUTE_FILE, &store) != FS_ERR_NO_ERROR) {
-    nx_display_string("Error opening data file!");
+  nx_display_string("Progr. page #");
+  nx_display_uint(num);
+  nx_display_end_line();
+
+  nx_display_string("<< ");
+  nx_display_uint(FLASH_BASE_PTR[num*EFC_PAGE_WORDS]);
+  nx_display_end_line();
+
+  data[0] = FLASH_BASE_PTR[num*EFC_PAGE_WORDS]+1;
+  nx_display_string("data[0] = ");
+  nx_display_uint(data[0]);
+  nx_display_end_line();
+
+  nx_display_string("CMD: ");
+  nx_display_hex(t);
+  nx_display_end_line();
+  
+  if (!nx__efc_write_page(data, num)) {
+    nx_display_string("Failed!\nPress CANCEL to halt");
+    while (nx_avr_get_button() != BUTTON_CANCEL);
     return;
   }
-  
-  nx_display_string("File opened (fd:");
-  nx_display_uint(store);
-  nx_display_string(")\n");
+
+  nx_display_string("Press OK to read.\n");
+
+  while (nx_avr_get_button() != BUTTON_OK);
+
+  nx_display_string(">> ");
+  nx_display_uint(FLASH_BASE_PTR[num*EFC_PAGE_WORDS]);
+  nx_display_end_line();
+
+  while (nx_avr_get_button() != BUTTON_CANCEL);
 }
 
