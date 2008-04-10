ca6c167f89630dfbcb8b68799be8ef92fd09fc7c
Maxime Petazzoni <maxime.petazzoni@bulix.org>
2008-04-10 16:59:14 +0200

    Demonstrate the menu scrolling feature.

--- nxos/systems/guitest/main.c
+++ nxos/systems/guitest/main.c
@@ -11,21 +11,22 @@
 #include "base/lib/gui/gui.h"
 
 void main(void) {
-  char *entries[] = {"Browse", "Sysinfo", "Settings", "Halt", NULL};
+  char *entries[] = {"Browse", "Sysinfo", "Settings", "Format", "Item5", "Item6", "Item7", "Halt", NULL};
   gui_text_menu_t menu;
   U8 res;
 
   menu.entries = entries;
   menu.title = "Home menu";
-  menu.active_mark = "->";
+  menu.active_mark = "> ";
 
   while (TRUE) {
     res = nx_gui_text_menu(menu);
     nx_display_end_line();
 
     switch (res) {
-      case 3:
-        nx_display_string("Halting...");
+      case 7:
+        nx_display_clear();
+        nx_display_string(">> Halting...");
         nx_systick_wait_ms(1000);
         return;
         break;
