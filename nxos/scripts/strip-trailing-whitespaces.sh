1489b112170c25a80708ed87666789109da63ec7
David Anderson <dave@natulte.net>
2008-05-31 23:58:31 +0200

    Make the whitespace stripper script also remove tabs.
    Only the GNU sed variant does this, since OSX sed seems to exhibit
    more signs of brain damage.

--- nxos/scripts/strip-trailing-whitespaces.sh
+++ nxos/scripts/strip-trailing-whitespaces.sh
@@ -11,6 +11,9 @@ if [ "$SYSTEM" = "Darwin" ]; then
   # simply retarded. -i requires an extension, and there is no way
   # that we can see to not have it generate backups. What does one do
   # then? Give it the finger.
+  #
+  # TODO: OSX sed apparently fails to catch tabs with the expression
+  # that works with GNU sed. Figure out a way around that.
   for i in `find . -name '*.[chS]'`; do
     sed -i ".re-bak" -E -e 's/ +$//g' $i
   done
@@ -19,7 +22,7 @@ else
   # Assume GNU sed by default. Nice, sane, normal GNU sed.
   for i in `find . -name '*.[chS]'`; do
     echo $i
-    sed -i -r -e 's/ +$//g' $i
+    sed -i -r -e 's/[ \t]+$//g' $i
   done
 fi
 
