ab92b28a6063551920d39041bc43d2dde9dfa27d
Maxime Petazzoni <maxime.petazzoni@bulix.org>
2008-05-31 21:30:52 +0200

    Add a trailing whitespaces stripping utility.

---
+++ nxos/scripts/strip-trailing-whitespaces.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# Trailing whitespace is the work of the devil and VI users. This
+# script scans all source code files and strips out trailing
+# whitespace.
+
+SYSTEM=`uname -s`
+
+if [ "$SYSTEM" = "Darwin" ]; then
+  # Apple's sed is RETARDED. There is no other way to say this, it is
+  # simply retarded. -i requires an extension, and there is no way
+  # that we can see to not have it generate backups. What does one do
+  # then? Give it the finger.
+  for i in `find . -name '*.[chS]'`; do
+    sed -i ".re-bak" -E -e 's/ +$//g' $i
+  done
+  find . -name '*.re-bak' -print0 | xargs -0 rm
+else
+  # Assume GNU sed by default. Nice, sane, normal GNU sed.
+  for i in `find . -name '*.[chS]'`; do
+    echo $i
+    sed -i -r -e 's/ +$//g' $i
+  done
+fi
+
