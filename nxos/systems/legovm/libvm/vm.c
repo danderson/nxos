3486a3470dedc203f9bed4f74855bd262f73eb1c
David Anderson <dave@natulte.net>
2008-06-22 07:00:57 +0200

    Followup tweaks to the merge of vm.c and vm_array.c.

--- nxos/systems/legovm/libvm/vm.c
+++ nxos/systems/legovm/libvm/vm.c
@@ -14,25 +14,6 @@
 #include "vm.h"
 #include "vm_header.h"
 
-static const U8 magic[14] = "MindstormsNXT";
-static const U16 version = 0x500;
-
-/* Mapping from data types to size in bytes. VOID and CLUSTER are
- * listed as size 0 because they are irrelevant for this mapping.
- */
-const U8 data_type_size[NUM_DATA_TYPES] = {
-  0, /* VOID */
-  1, /* UBYTE */
-  1, /* SBYTE */
-  2, /* UWORD */
-  2, /* SWORD */
-  4, /* ULONG */
-  4, /* SLONG */
-  2, /* ARRAY */
-  0, /* CLUSTER */
-  4  /* MUTEX */
-};
-
 /* The actual VM state. */
 vm_state vm;
 
@@ -72,7 +53,7 @@ static const dstoc_record *init_array(const dstoc_record *rec,
   /* Write the offset into our vector array into the static dataspace
    * instead of the DV index.
    */
-  *((U16*)&vm.ds_static[rec->data]) = vec - vm.arrays;
+  *((U16*) &vm.ds_static[rec->data]) = vec - vm.arrays;
 
   /* Skip past all records that define the inner type of the
    * array. This is only actually relevant if the inner type is
@@ -91,10 +72,10 @@ static const dstoc_record *init_array(const dstoc_record *rec,
   return rec;
 }
 
-static void init_static_dataspace() {
+static void init_dataspace() {
   const dstoc_record *rec = vm.dstoc;
   const dstoc_record *end = vm.dstoc + vm.header->dstoc_entry_count;
-  const U8 *static_defaults = (U8*)end;
+  const U8 *static_defaults = (U8*) end;
   const U8 *dynamic_defaults =
     static_defaults + vm.header->defaults_dynamic_offset;
   const dopevector *dva = get_dopevector(dynamic_defaults,
@@ -117,7 +98,7 @@ static void init_static_dataspace() {
 
     case ARRAY:
       rec = init_array(rec, dynamic_defaults, dva,
-                       *((U16*)static_defaults), vec++);
+                       *((U16*) static_defaults), vec++);
       static_defaults += size;
       break;
 
@@ -131,18 +112,29 @@ static void init_static_dataspace() {
       rec++;
     }
   }
+
+  /* The number of arrays actually used may be smaller than the number
+   * initially allocated. If necessary, downsize the array vector.
+   */
+  U32 num_vecs = (U32) (vec - vm.arrays);
+  if (num_vecs < vm.num_arrays) {
+    vm.num_arrays = num_vecs;
+    vm.arrays = nx_realloc(vm.arrays, vm.num_arrays * sizeof(vector));
+  }
 }
 
 bool lego_vm_init(const U8 *program) {
-  vm.header = (const rxe_header*) program;
+  const U8 magic[14] = "MindstormsNXT";
+  const U16 version = 0x500;
 
-  if (!streq((char*)vm.header->magic, (char*)magic) ||
+  vm.header = (const rxe_header*) program;
+  if (!streq((char*) vm.header->magic, (char*) magic) ||
       vm.header->version != version)
     return FALSE;
 
   vm.dstoc = (const dstoc_record*) (program + sizeof(rxe_header));
   vm.ds_static = nx_malloc(vm.header->ds_initial_static_size);
 
-  init_static_dataspace();
+  init_dataspace();
   return TRUE;
 }
