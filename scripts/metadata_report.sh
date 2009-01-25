105adc1612189a70306ecdf6aa406ddb9d2606a4
David Anderson <dave@natulte.net>
2009-01-25 05:18:40 +0100

    Update metadata_report.sh to work with git.

--- scripts/metadata_report.sh
+++ scripts/metadata_report.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2008 the NxOS developers
+# Copyright (c) 2008,2009 the NxOS developers
 #
 # See AUTHORS for a full list of the developers.
 #
@@ -11,31 +11,15 @@
 #  - Author is the auto-guessed email, instead of a nice name.
 #  - Commit message is way too short.
 
-AUTHOR=`hg tip --template {author}`
-AUTHOR_VALID=`echo $AUTHOR | egrep -i '[a-z ]+ (\[[a-z0-9 ]+\] )?<[a-z.+@]+>'`
+AUTHOR=`git config --get user.name`
+EMAIL=`git config --get user.email`
 
-if [ "x$AUTHOR_VALID" = "x" ]; then
-  echo "Changeset author does not conform to the pattern Firstname Lastname <email>"
-  echo " eg. John Smith <jsmith@example.com>"
-  echo "Please add the ui:username variable to you ~/.hgrc. Here is a sample .hgrc:"
-  echo
-  echo "[ui]"
-  echo "username = John Smith <jsmith@example.com>"
-  echo
-  hg tip --template {desc} >commit.msg
-  echo "Commit message saved in commit.msg"
-  exit 1
-fi
-
-MSG_OK=`hg tip --template {desc} | wc -c`
-
-if [ $MSG_OK -lt 15 ]; then
-  echo "Your commit message is very short, and probably doesn't describe your"
-  echo "change very well. Please be a little more verbose."
-  echo
-  hg tip --template {desc} >commit.msg
-  echo "Commit message saved in commit.msg"
-  exit 1
+if [ "x$AUTHOR" = "x" -o "x$EMAIL" = "x" ]; then
+    echo "Author or email unspecified in configuration."
+    echo "Please configure both by running:"
+    echo "  git config --global user.name \"John Doe\""
+    echo "  git config --global user.email \"john.doe@example.com\""
+    exit 1
 fi
 
 exit 0
