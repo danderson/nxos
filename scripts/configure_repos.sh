878baa818366472efa1ff38bac804c2ab20a9afa
David Anderson <dave@natulte.net>
2009-01-25 05:42:08 +0100

    Add a configure_repos script that works with git.

---
+++ scripts/configure_repos.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#
+# Copyright (c) 2009 the NxOS developers
+#
+# See AUTHORS for a full list of the developers.
+#
+# Redistribution of this file is permitted under
+# the terms of the GNU Public License (GPL) version 2.
+#
+# Sets up a freshly cloned repository with a few useful things.
+
+REPOS_URL="git://natulte.net/nxos"
+
+echo `pwd`
+
+if [ ! -f "scripts/configure_repos.sh" ]; then
+  echo "Please run from the root of your repository."
+  exit 1
+fi
+
+git remote add devel $REPOS_URL/devel
+
+for name in ayolo dave jflesch sam; do
+    git remote add $name $REPOS_URL/crew/$name
+done
+
+cat <<EOF >.git/hooks/pre-commit
+#!/bin/bash
+
+ALL_OK=1
+
+check_result () {
+  if [ \$? -ne 0 ]; then
+    ALL_OK=0
+    echo
+  fi
+}
+
+./scripts/copyright_report.py
+check_result
+
+./scripts/trailing_whitespace_report.py
+check_result
+
+./scripts/metadata_report.sh
+check_result
+
+if [ \$ALL_OK -ne 1 ]; then
+  echo "Precommit hooks failed, see above."
+  exit 1
+else
+  exit 0
+fi
+EOF
+
+chmod +x .git/hooks/pre-commit
