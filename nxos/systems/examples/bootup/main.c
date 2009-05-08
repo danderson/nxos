dbab8890c01e5807f0479b82cb4a9da7bea3f187
David Anderson <danderson@izar.(none)>
2009-05-08 16:18:38 +0200

    Inaugurate the examples/ subtree with a trivial boot-and-die kernel.

---
+++ nxos/systems/examples/bootup/main.c
@@ -0,0 +1,19 @@
+/* Copyright (c) 2009 the NxOS developers
+ *
+ * See AUTHORS for a full list of the developers.
+ *
+ * Redistribution of this file is permitted under
+ * the terms of the GNU Public License (GPL) version 2.
+ */
+
+/* Trivial bootup example.
+ *
+ * This example illustrates the structure of a trivial application
+ * kernel, which does nothing other than booting up, then immediately
+ * powering down again. This file and its associated SConscript is the
+ * minimal structure that you need to start building your own
+ * application kernel.
+ */
+
+void main() {
+}
